d48e81943431d318133b243b65ab3b37123aeee3
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-02-07 18:33:05 -0500

    defined some variables for password

--- script.js
+++ script.js
@@ -18,4 +18,10 @@ generateBtn.addEventListener("click", writePassword);
 
 
 // Write function for generatePassword
-let chars = "0123456789abcdefghijklmnopqrstuvwxyz!#$%&'()*+,-./:;<=>?@[\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let numbers = [0,1,2,3,4,5,6,7,8,9]
+let lowerCase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+let upperCase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+let specialCharacters = [!,#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,\\,\],^,_,`,{,|,},~]
+
+
