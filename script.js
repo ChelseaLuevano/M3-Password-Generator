9e907084257e6c66cc7e157697610140e9eda77e
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-02-10 07:45:53 -0600

    completed generate password functionality, created password variable, updated password length criteria to validate it is a number

--- script.js
+++ script.js
@@ -2,52 +2,79 @@
 
 
 //assign variables for Password criteria
-// let validnumbers = [0,1,2,3,4,5,6,7,8,9];
-// let validLowerCase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
-// let validUpperCaseO = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
-// let specialCharacters = ['!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_',',','{','|','}','~'];
+let validNumbers = [0,1,2,3,4,5,6,7,8,9];
+let validLowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let validUpperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let validSpecialCharacters = ['!','#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_',',','{','|','}','~'];
 
-// ''' special character that I need to add to array
-
+// random number between 0 - length of array
+function getRandom(arr) {
+  return arr[Math.floor(Math.random()* arr.length)]
+}
 
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
 
 function generatePassword () {
-  console.log("Hey you clicked the button")
+  let password = "";
+  let possibleCharacters = [];
+  console.log("Hey you clicked the button");
+
 // 1. Prompt user for Password Criteria
 
 // a.   Prompt user for Password Length
-let userLength = window.prompt("Enter a Password length between '8' and '128' characters.");
+let userLength = parseInt(prompt("Enter a Password length between '8' and '128' characters."));
 // a.1  Validate the input 
-if (userLength < 8 || userLength > 128) {
-    return window.prompt("Please enter a Password length between 8 and 128 characters.");
-
+while ((userLength < 8) || (userLength > 128) || isNaN(userLength)){
+  userLength = parseInt(prompt("Enter a Password length between '8' and '128' characters."));
+}
 
 // b.   Prompt user for Upper Case, Lower Case, Numeric, and Special Chracters
 // b1. Upper Case 
 
-} else (userLength >=8  && userLength <= 128) 
-{ let upperCase = window.confirm("To include Upper Case Letters in your Password, click 'ok'; Otherwide, click 'cancel'. ");
-  // upperCase();
-}
-// b2. Validate the input
-function upperCase (){
-if (upperCase === true) { 
-  let lowerCase = window.confirm("To include Lower Case Letters in your Password, click 'ok'; Otherwide, click 'cancel'. ");
-  };
+  let upperCase = confirm("To include Upper Case Letters in your Password, click 'ok'; Otherwide, click 'cancel'. ");
 
-// else (upperCase === false)
-}
- upperCase();
+// b2. Lower Case
+
+  let lowerCase = confirm("To include Lower Case Letters in your Password, click 'ok'; Otherwide, click 'cancel'. ");
+
+// numeric promts
+let numeric = confirm("To include numbers in your Password, click 'ok'; Otherwide, click 'cancel'. ");
+
+// special character prompts
+
+let specialCharacters = confirm("To include Special Characters in your Password, click 'ok'; Otherwide, click 'cancel'. ");
 
 // 2. Validate the input
 
 // 3. Generate Password
+if (upperCase) {
+  possibleCharacters = [...possibleCharacters, ...validUpperCase];
+  password += getRandom(validUpperCase);
+}
+
+if (lowerCase) {
+  possibleCharacters = [...possibleCharacters, ...validLowerCase];
+  password += getRandom(validLowerCase);
+}
+
+if (numeric) {
+  possibleCharacters = [...possibleCharacters, ...validNumbers];
+  password += getRandom(validNumbers);
+}
+
+if (specialCharacters) {
+  possibleCharacters = [...possibleCharacters, ...validSpecialCharacters];
+  password += getRandom(validSpecialCharacters);
+}
+
+for (let i = password.length; i < userLength; i++) {
+  password += getRandom(possibleCharacters);
+}
 
 // 4. Display password
-  return "Generated Password will go here!"
+  return password
 }
 
 // Write password to the #password input
